bc5fd412ce433843790654165c5a8b5f065cf3a3
Сергей <domovomy@rambler.ru>
2020-01-14 18:58:49 +0200

    works

--- Task3/index.js
+++ Task3/index.js
@@ -1,6 +1,6 @@
 const in_data = require('../Task3/in_data.json');
+const algorithm = require('../Task3/genAlgorithm');
 
+//algorithm(in_data.corrections,in_data.cells);
 
-
-console.log(in_data.corrections);
-console.log(in_data.cells);
+algorithm ([2, 10, 5, 7, 8, 1, 1, 3, 4, 2]);
